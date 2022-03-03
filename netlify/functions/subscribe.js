51599b8aefd71a5ee095b73a7e9c9f0b67e471dc
async <100706802+not-sync@users.noreply.github.com>
2022-03-02 19:38:46 -0500

    feat: add basic request for subscription

---
+++ netlify/functions/subscribe.js
@@ -0,0 +1,28 @@
+const axios = require("axios").default;
+
+var qs = require("qs");
+var data = qs.stringify({
+  "hub.callback": process.env.CALLBACK,
+  "hub.mode": "subscribe",
+  "hub.topic":
+    "https://www.youtube.com/xml/feeds/videos.xml?channel_id=" +
+    process.env.CHANNEL_ID,
+});
+var config = {
+  method: "post",
+  url: "https://pubsubhubbub.appspot.com/subscribe",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+  data: data,
+};
+
+exports.handler = function (event, context, callback) {
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,7 +1,7 @@
 exports.handler = async (event, context) => {
-  console.log(event.body, event.httpMethod);
+  console.log(event.body);
   return {
     statusCode: 200,
-    body: "Hello, World",
+    body: event.body.challenge,
   };
 };
