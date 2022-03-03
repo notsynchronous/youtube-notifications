9ffb209d6da79d110b2cc574d9a2d3decb2fb04d
async <100706802+not-sync@users.noreply.github.com>
2022-03-02 19:54:32 -0500

    fix: update response variable

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,7 +1,6 @@
 exports.handler = async (event, context) => {
-  console.log(event, context);
   return {
     statusCode: 200,
-    body: event.body.challenge,
+    body: event.queryStringParameters["hub.challenge"],
   };
 };
